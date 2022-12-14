cba3d41d2e52529c7fec4b152702ff008c6af506
Alex <sterx0@gmail.com>
2022-12-14 17:41:44 +0300

    pause

--- base_const_2/less2_3/less2_3.py
+++ base_const_2/less2_3/less2_3.py
@@ -1,18 +1,34 @@
-# N Простая задача ///////////
-
-num = int(input())
-
-lst = [2]
-
-for i in range(3, num + 1, 2):
-    if (i > 10) and (i % 10 == 5):
-        continue
-    for j in lst:
-        if j*j-1 > i:
-            lst.append(i)
-            break
-        if i % j == 0:
-            break
-    else:
-        lst.append(i)
-print(lst)
+# # N Простая задача ///////////
+#
+# num = int(input())
+#
+# lst = [2]
+#
+# for i in range(3, num + 1, 2):
+#     if (i > 10) and (i % 10 == 5):
+#         continue
+#     for j in lst:
+#         if j*j-1 > i:
+#             lst.append(i)
+#             break
+#         if i % j == 0:
+#             break
+#     else:
+#         lst.append(i)
+# print(lst)
+#
+# Flag == False
+#
+# while num > 2:
+#     if num == 2:
+#         Flag = True
+#         break
+#     d = 3
+#     for i in range(d, num+1, 2):
+#         if i > 10 and i % 10 == 0:
+#             break
+#         for j
+#
+#         if i * i > num:
+#             Flag = True
+#             break
