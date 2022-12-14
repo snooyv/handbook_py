f6cb839c36b28be29cbffb6c8b49b3c5b1dd2c6d
Alex <sterx0@gmail.com>
2022-12-14 23:55:22 +0300

    B test ok

--- collections3/less3_1.py
+++ collections3/less3_1.py
@@ -1,16 +1,24 @@
 # A Азбука ///////////
 
-vol_word = int(input())
+# vol_word = int(input())
+#
+# is_has = "YES"
+#
+# while vol_word > 0:
+#     data = input()
+#
+#     vol_word -= 1
+#     if is_has == "NO":
+#         continue
+#     if not data.startswith("а") and not data.startswith("б") and not data.startswith("в"):
+#         is_has = "NO"
+#
+# print(is_has)
 
-is_has = "YES"
 
-while vol_word > 0:
-    data = input()
+# B Кручу-верчу ////////
 
-    vol_word -= 1
-    if is_has == "NO":
-        continue
-    if not data.startswith("а") and not data.startswith("б") and not data.startswith("в"):
-        is_has = "NO"
+# strng = input()
 
-print(is_has)
+for i in input():
+    print(i)
