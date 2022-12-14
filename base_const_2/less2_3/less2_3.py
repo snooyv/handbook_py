2ad3d461f0b17ba9f484aab6a8848b929a09f3d7
Alex <sterx0@gmail.com>
2022-12-14 14:36:12 +0300

    first solution

---
+++ base_const_2/less2_3/less2_3.py
@@ -0,0 +1,18 @@
+# N Простая задача ///////////
+
+num = int(input())
+
+lst = [2]
+
+for i in range(3, num + 1, 2):
+    if (i > 10) and (i % 10 == 5):
+        continue
+    for j in lst:
+        if j*j-1 > i:
+            lst.append(i)
+            break
+        if i % j == 0:
+            break
+    else:
+        lst.append(i)
+print(lst)
