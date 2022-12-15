462897eaac4eeac7e3bdcc5c2785c7adaf37bf72
Alex <sterx0@gmail.com>
2022-12-15 20:22:22 +0300

    E test ok

--- collections3/less3_1.py
+++ collections3/less3_1.py
@@ -47,17 +47,25 @@
 
 # D Очистка данных /////////
 
-start_char = "##"
-end_char = "@@@"
-data = []
+# start_char = "##"
+# end_char = "@@@"
+# data = []
+#
+# while (string := input()) != "":
+#     if string.endswith(end_char):
+#         continue
+#     elif string.startswith(start_char):
+#         data.append(string[2:])
+#     else:
+#         data.append(string)
+#
+# for i in data:
+#     print(i)
 
-while (string := input()) != "":
-    if string.endswith(end_char):
-        continue
-    elif string.startswith(start_char):
-        data.append(string[2:])
-    else:
-        data.append(string)
 
-for i in data:
-    print(i)
+# E А роза упала на лапу Азоро 4.0
+
+string = input()
+
+print("YES") if string == string[::-1] else print("NO")
+
